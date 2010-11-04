50c335db4ae1e504930dee24f62eac4634ed8c5a
jason.l.forbes <jason.l.forbes@gmail.com>
2010-11-04 21:42:00 +0000

    Cleaning up code

---
+++ InfoRanch/branches/jforbes1103/Classes/DBTemplate.vb
@@ -0,0 +1,56 @@
+﻿'Class to hold a database item template
+'name = Database name
+'field = field name
+'dataType = datatype of field
+'sortOrder = sort order of the fields in the DB
+'length = number of fields in template
+'setName(string) = sets the name variable
+'addItem(string,string,integer) = adds the field name and datatype
+'getName() = returns the name variable
+'getField(int) = returns the field name at the specified index
+'getDataType(int) = returns the data type for the field at the specified index
+'getSortOrder(int) = returns the sort order number for the field at the specified index
+Namespace DB
+
+    Public Class DBTemplate
+        'Member variables
+        Private name As String
+        Private field As New ArrayList
+        Private dataType As New ArrayList
+        Private sortOrder As New List(Of Integer)
+        Public length As Integer = 0
+
+        'Member Functions
+        Public Sub setName(ByVal dbName As String)
+            name = dbName 'Set name variable to parameter
+        End Sub
+
+        Public Sub addItem(ByVal fName As String, ByVal dType As String, ByVal sO As Integer)
+            'Takes 2 strings, the field name, and the data type and stores them in the
+            'correct ArrayList.  Increments the length by 1.
+            field.Add(fName)
+            dataType.Add(dType)
+            sortOrder.Add(sO)
+            length = length + 1
+
+        End Sub
+
+        'Return the values specified.  getField and getDataType take an integer index
+        Public Function getName() As String
+            Return name
+        End Function
+
+        Public Function getField(ByVal index As Integer) As String
+            Return field(index)
+        End Function
+
+        Public Function getDataType(ByVal index As Integer) As String
+            Return dataType(index)
+        End Function
+
+        Public Function getSortOrder(ByVal index As Integer) As Integer
+            Return sortOrder(index)
+        End Function
+    End Class
+End Namespace
+
---
+++ InfoRanch/branches/jforbes1103/MemberPages/DatabaseTemplates.aspx.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Partial Public Class DatabaseTemplates
+    Inherits System.Web.UI.Page
+
+    Dim DBCmd = New SqlCommand
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+
+        ' connects to the templates database
+
+        SqlDataSource1.ConnectionString = "Persist Security Info=False;Integrated Security=SSPI;" & _
+                                  "database=templates_database;server=localhost;Connect Timeout=30"
+
+
+        ' queries the database names and loads the results to the radio button list
+        SqlDataSource1.SelectCommand = "SELECT fields FROM template_names"
+
+
+
+
+       
+    End Sub
+
+    
+
+    Protected Sub SubmitBTN_Click(ByVal sender As Object, ByVal e As EventArgs) Handles SubmitBTN.Click
+
+        ' assigns the value to the session variable
+
+        Session("template_selection") = templateList.SelectedValue
+
+        Server.Transfer("~/MemberPages/TemplatePage.aspx", True)
+
+
+
+
+    End Sub
+End Class
+
+
+
+
+
